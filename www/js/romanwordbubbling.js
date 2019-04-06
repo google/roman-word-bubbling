d10159135ec7b7dbf60fade83209091d0e429647
TommySzalapski <Tommy.Szalapski@gmail.com>
2019-04-06 15:50:52 -0600

    Remove spurious semicolons since this is javascript

--- www/js/romanwordbubbling.js
+++ www/js/romanwordbubbling.js
@@ -19,8 +19,8 @@
 var fontName = "'Comic Sans MS'"
 
 document.getElementById("textInput").addEventListener('keyup', function (){
-    renderImage();
-}, false);
+    renderImage()
+}, false)
 renderImage()
 
 function updateFont() {
@@ -83,83 +83,83 @@ function loadCustomFont() {
   }
 }
 function renderImage() {
-  let fontSize = parseInt(document.getElementById('fontSize').value, 10);
+  let fontSize = parseInt(document.getElementById('fontSize').value, 10)
   // Set gapWidth and outlineThickness as a percentage of the font size
-  let gapWidth = fontSize * parseInt(document.getElementById('gapWidth').value, 10) / 100;
-  let outlineThickness = fontSize * parseInt(document.getElementById('outlineThickness').value, 10) / 100;
-  let padding = fontSize / 4;
+  let gapWidth = fontSize * parseInt(document.getElementById('gapWidth').value, 10) / 100
+  let outlineThickness = fontSize * parseInt(document.getElementById('outlineThickness').value, 10) / 100
+  let padding = fontSize / 4
   let removeText = document.getElementById('removeText').checked
   let darkMode = document.getElementById('darkMode').checked
   let text = document.getElementById('textInput').value
-  var tCtx = document.getElementById('textCanvas').getContext('2d'); //Hidden canvas
-  let blurRadius = parseInt(document.getElementById('blurRadius').value, 10);
+  var tCtx = document.getElementById('textCanvas').getContext('2d') //Hidden canvas
+  let blurRadius = parseInt(document.getElementById('blurRadius').value, 10)
   let borderColorHex = document.getElementById("borderColor").value
 
   tCtx.font = fontSize + "px " + fontName
-  tCtx.canvas.width = tCtx.measureText(text).width + padding*2;
-  tCtx.canvas.height = 1.25*fontSize + 2*padding;
+  tCtx.canvas.width = tCtx.measureText(text).width + padding*2
+  tCtx.canvas.height = 1.25*fontSize + 2*padding
   tCtx.font = fontSize + "px " + fontName
-  tCtx.fillStyle = 'white';
-  tCtx.fillRect(0, 0, tCtx.canvas.width, tCtx.canvas.height);
-  tCtx.fillStyle = 'black';
-  tCtx.fillText(text, padding, fontSize + padding/2);
-  let img = cv.imread('textCanvas');
-  let shape = cv.Mat.zeros(img.cols, img.rows, cv.CV_8UC1);
-  cv.cvtColor(img, shape, cv.COLOR_RGBA2GRAY, 0);
-  cv.bitwise_not(shape, shape);
+  tCtx.fillStyle = 'white'
+  tCtx.fillRect(0, 0, tCtx.canvas.width, tCtx.canvas.height)
+  tCtx.fillStyle = 'black'
+  tCtx.fillText(text, padding, fontSize + padding/2)
+  let img = cv.imread('textCanvas')
+  let shape = cv.Mat.zeros(img.cols, img.rows, cv.CV_8UC1)
+  cv.cvtColor(img, shape, cv.COLOR_RGBA2GRAY, 0)
+  cv.bitwise_not(shape, shape)
 
   // Make white image for border
-  let borderImage = cv.Mat.zeros(img.rows, img.cols, cv.CV_8UC3);
-  cv.bitwise_not(borderImage, borderImage);
+  let borderImage = cv.Mat.zeros(img.rows, img.cols, cv.CV_8UC3)
+  cv.bitwise_not(borderImage, borderImage)
 
   // Make non-transparent image for text
-  let textImage = cv.Mat.zeros(img.rows, img.cols, cv.CV_8UC3);
-  cv.cvtColor(img, textImage, cv.COLOR_RGBA2RGB, 0);
+  let textImage = cv.Mat.zeros(img.rows, img.cols, cv.CV_8UC3)
+  cv.cvtColor(img, textImage, cv.COLOR_RGBA2RGB, 0)
 
-  let contours = new cv.MatVector();
-  let hierarchy = new cv.Mat();
-  let contourImage = cv.Mat.zeros(img.rows, img.cols, cv.CV_8UC3);
+  let contours = new cv.MatVector()
+  let hierarchy = new cv.Mat()
+  let contourImage = cv.Mat.zeros(img.rows, img.cols, cv.CV_8UC3)
 
   // Find and draw contours
   // RETR_EXTERNAL means it will fill in holes in letters like 'o' and 'a'
   // Draw thickly enough that the outside edge will be the center of the outline
-  cv.findContours(shape, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
-  let color = null;
+  cv.findContours(shape, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+  let color = null
   r = parseInt(borderColorHex.substring(1, 3), 16)
   g = parseInt(borderColorHex.substring(3, 5), 16)
   b = parseInt(borderColorHex.substring(5, 7), 16)
   if (darkMode) {
     // Invert the color for dark mode because it will get inverted back later
     // Doing it this way ensures the blurring will use the right background color
-    color = new cv.Scalar(255-r, 255-g, 255-b);
+    color = new cv.Scalar(255-r, 255-g, 255-b)
   } else {
-    color = new cv.Scalar(r, g, b);
+    color = new cv.Scalar(r, g, b)
   }
-  cv.drawContours(contourImage, contours, -1, color, gapWidth + outlineThickness);
+  cv.drawContours(contourImage, contours, -1, color, gapWidth + outlineThickness)
 
   // Flatten contour image into a grayscale image and make it white-on-black also
-  cv.cvtColor(contourImage, shape, cv.COLOR_BGR2GRAY);
-  cv.threshold(shape, shape, 0, 255, cv.THRESH_BINARY);
+  cv.cvtColor(contourImage, shape, cv.COLOR_BGR2GRAY)
+  cv.threshold(shape, shape, 0, 255, cv.THRESH_BINARY)
 
   // Find the outside edge of the countour we just drew
   // This will be the center of the outline
-  cv.findContours(shape, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+  cv.findContours(shape, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
   // Add outline to original image
-  cv.drawContours(borderImage, contours, -1, color, outlineThickness);
+  cv.drawContours(borderImage, contours, -1, color, outlineThickness)
 
   // Blur the border image to make it look less pixelated
-  cv.GaussianBlur(borderImage, borderImage, new cv.Size(blurRadius, blurRadius), 0, 0, cv.BORDER_DEFAULT);
+  cv.GaussianBlur(borderImage, borderImage, new cv.Size(blurRadius, blurRadius), 0, 0, cv.BORDER_DEFAULT)
 
   if(!removeText) {
     // Combine the text and the border
     cv.bitwise_and(borderImage, textImage, borderImage)
   }
   if (darkMode) {
-    cv.bitwise_not(borderImage, borderImage);
+    cv.bitwise_not(borderImage, borderImage)
   }
 
-  cv.imshow('output', borderImage);
+  cv.imshow('output', borderImage)
   img.delete(); shape.delete(); contours.delete(); hierarchy.delete();
   contourImage.delete(); textImage.delete(); borderImage.delete();
 }
